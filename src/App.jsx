231324bd9debf5b6a498176ae556263409e0f6d0
AkiraYoshida <akira.y523@outlook.com>
2016-10-01 04:23:56 +0900

    improve modal content style 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -25,72 +25,65 @@ export default class App extends React.Component {
   render(){
     return (
       <Page>
-        <Button style={{marginLeft:20}} ref='button' onClick={() => this.setState({isOpen: true})} > CLICK HERE! 
+        <Button style={styles.ModalClickBtn} ref='button' onClick={() => this.setState({isOpen: true})} > CLICK HERE! 
         </Button>
-        <Modal style={{}} isOpen={this.state.isOpen}>
+        <Modal isOpen={this.state.isOpen}>
 
-          <div style={{backgroundColor:'white', width:500, textAlign:'left'}}>
-            <div style={{marginTop:20, marginLeft:20, paddingTop:30}}>
-              <p style={{color:'black', fontSize:20}}>Join the Action Sports Network</p>
+          <div style={styles.bg}>
+            <div style={styles.TitleDiv}>
+              <p style={styles.Title}>Join the Action Sports Network</p>
             </div>
 
-            <div style={{marginTop:50, padding:20}}>
-              <p style={{fontSize:15,color:'black'}}> 
+            <div style={styles.TitleContentDiv}>
+              <p style={styles.ContentTitle}> 
                 Lorem ipsum dolor sit asmet, consectectur adipiscing elit. Nulla neque nibh, consequat in orci id, course auctor tortor. Fusce viverra bibendum erat, non rhoncus maris interdum in. In sit amet vehicula diam.
               </p>
             </div>
-
-            <div style={{marginTop:20, marginLeft:15}}>
-              <p style={{color:'black', fontSize:25}}>Join With Your Facebook Account</p>
+            <div style={styles.FBSignTitleDiv}>
+              <p style={styles.FBSignTitle}>Join With Your Facebook Account</p>
+              <p style={styles.FBSignTitle}>Or</p>
+              <p style={styles.FBSignTitle}> Fill Out Ehe Form Below </p>
             </div>
 
-            <div style={{marginTop:20, marginLeft:15}}>
-              <p style={{color:'black'}}>Or</p>
-            </div>
-
-            <div style={{marginTop:20, marginLeft:15}}>
-              <p style={{color:'black', fontSize:25}}> Fill Out Ehe Form Below </p>
-            </div>
-            <section style={{paddingBottom:50}}>
-              <div style={{marginLeft:20, marginTop:20, marginBottom:10}}>
-                <Input style={{width:130, backgroundColor:'#eee',paddingLeft:5}}
+            <section style={styles.TextInputBg}>
+              <div style={styles.FirstTextDiv}>
+                <Input style={styles.LineFirstBox}
                   value={this.state.text} float
-                  onChange={(event) => { this.setState({firstName: event.target.value})} }
+                  onChange={(event) => { this.setState({firstName: event.target.value})}}
                   placeholder='FirstName*'/>
-                <Input  style={{width:130, backgroundColor:'#eee',marginLeft:15, paddingLeft:5}}
+                <Input  style={styles.LineSecBox}
                   value={this.state.text} float
                   onChange={(event) => { this.setState({lastName: event.target.value})} }
                   placeholder='LastName*' />
               </div>
-              <div style={{marginLeft:20}}>
-                <Input style={{width:130, backgroundColor:'#eee',paddingLeft:5}}
+              <div style={styles.SecondTextDiv}>
+                <Input style={styles.LineFirstBox}
                   value={this.state.text} float
                   onChange={(event) => { this.setState({userName: event.target.value})}}
                   placeholder='UserName*' />
-                <Input style={{width:130, backgroundColor:'#eee', marginLeft:15, paddingLeft:5}}
+                <Input style={styles.LineSecBox}
                   value={this.state.text} float
                   onChange={(event) => { this.setState({emailAddress: event.target.value})} }
                   placeholder='Email Address*' />
               </div>
-              <div style={{marginLeft:20, marginTop:20}}>
-                <Input style={{width:130, backgroundColor:'#eee',paddingLeft:5}}
+              <div style={styles.ThirdTextDiv}>
+                <Input style={styles.LineFirstBox}
                   value={this.state.text} float
                   onChange={(event) => { this.setState({password: event.target.value})} }
                   placeholder='Password*' />
-                <Input  style={{width:130, backgroundColor:'#eee',marginLeft:15, paddingLeft:5}}
-                  value={this.state.text} float
+                <Input  style={styles.LineSecBox}
                   onChange={(event) => { this.setState({confirmPassword: event.target.value})} }
                   placeholder='Confirm Password*' />
-                <Button style={{height:32, marginLeft:15,marginTop:10, paddingTop:0,backgroundColor:'#eee'}} onClick={this.alertPopup}> SUBMIT </Button>
+                <Button style={styles.SubmitBtn} onClick={this.alertPopup}> SUBMIT </Button>
               </div>
       
-              <div style={{marginLeft:20, marginTop:20}}>
+              <div style={styles.CheckBoxDiv}>
                 <label>
                     <Input
                       type='checkbox'
                     />
                 </label>
-                <label style={{marginLeft:10,color:'black'}}>I have read and agree to the</label>
+                <label style={styles.CheckBoxContent}>I have read and agree to the</label>
                 <a href="https://onsen.io/"> terms of sevice.*</a>
               </div> 
             </section>
@@ -102,10 +95,79 @@ export default class App extends React.Component {
   }
 }
 
-// const styles = StyleSheet.create({
+const styles = ({
 
-//   CustomInput:{
-//     width:200,
-//     backgroundColor:'#eee'
-//   }
-// });
+  ModalClickBtn:{
+    marginLeft:20
+  },
+  bg:{
+    backgroundColor:'white', 
+    textAlign:'left'
+  },
+  TitleDiv:{
+    marginTop:20, 
+    marginLeft:20, 
+    paddingTop:30
+  },
+  Title:{
+    color:'black', 
+    fontSize:20
+  },
+  TitleContentDiv:{
+    marginTop:50, 
+    padding:20
+  },
+  ContentTitle:{
+    fontSize:15,
+    color:'black'
+  },
+  TextInputBg:{
+    paddingBottom:50
+  },
+  FBSignTitleDiv:{
+    marginTop:20, 
+    marginLeft:15
+  },
+  FBSignTitle:{
+    color:'black', 
+    fontSize:25
+  },
+  FirstTextDiv:{
+    marginLeft:20, 
+    marginTop:20, 
+    marginBottom:10
+  },
+  LineFirstBox:{
+    width:130, 
+    backgroundColor:'#eee',
+    paddingLeft:5
+  },
+  LineSecBox:{
+    width:130, 
+    backgroundColor:'#eee',
+    marginLeft:15, 
+    paddingLeft:5
+  },
+  SecondTextDiv:{
+    marginLeft:20
+  },
+  ThirdTextDiv:{
+    marginLeft:20, 
+    marginTop:20
+  },
+  CheckBoxDiv:{
+    marginLeft:20, 
+    marginTop:20
+  },
+  CheckBoxContent:{
+    marginLeft:10,
+    color:'black'
+  },
+  SubmitBtn:{
+    height:32, 
+    marginLeft:15,
+    marginTop:10, 
+    paddingTop:0,
+    backgroundColor:'#eee'
+  }
+});
